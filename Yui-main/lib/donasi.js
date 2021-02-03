e1dcd1bd6c16acbc54df766e51e6929306c349aa
imld66 <cesarjuliotp@gmail.com>
2021-02-03 08:22:28 -0300

    First commit

---
+++ Yui-main/lib/donasi.js
@@ -0,0 +1,7 @@
+const donasi = () => { 
+	return `       
+Ao invés de doar, me segue no insta :3
+ImLD.gg
+}
+
+exports.donasi = donasi
---
+++ Yui-main/src/donasi.js
@@ -0,0 +1,11 @@
+const donasi = () => {
+	return `
+
+┏━━━━━━━━━━━━━━━━━━━━
+┃ Ao invés de doar, me segue no insta :3 ImLD.gg
+┣━━━━━━━━━━━━━━━━━━━
+
+`
+}
+
+exports.donasi = donasi
